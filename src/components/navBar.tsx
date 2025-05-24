5c337b2c9a84a99a3b000f4d36764cd2c4c545c5
Hardik <hardik@Neerajs-MacBook-Air.local>
2025-05-24 18:38:38 +0530

    vercel changes

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,5 +1,63 @@
+// import React from "react";
+// import { useLocation } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// import logo from "./img/logo.webp";
+
+// const Navbar: React.FC = () => {
+//   const location = useLocation();
+
+//   return (
+//     <nav
+//       className="navbar navbar-expand-lg bg-secondary navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn"
+//       data-wow-delay="0.1s"
+//     >
+//       <a href="/" className="navbar-brand ms-4 ms-lg-0">
+//         <h1 className="mb-0 text-primary text-uppercase">
+//           <img src={logo} alt="Sort Club Logo" width="20%" />
+//           SORT
+//         </h1>
+//       </a>
+//       <button
+//         type="button"
+//         className="navbar-toggler me-4"
+//         data-bs-toggle="collapse"
+//         data-bs-target="#navbarCollapse"
+//       >
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+//       <div className="collapse navbar-collapse" id="navbarCollapse">
+//         <div className="navbar-nav ms-auto p-4 p-lg-0">
+//           <a href="/" className="nav-item nav-link">
+//             Home
+//           </a>
+//           <a href="about" className="nav-item nav-link">
+//             About Us
+//           </a>
+//           <a href="events" className="nav-item nav-link">
+//             Our Events
+//           </a>
+//           <a href="magazine" className="nav-item nav-link">
+//             Magazine
+//           </a>
+//         </div>
+//         {location.pathname === "/" && (
+//           <a
+//             href="#contact"
+//             className="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block"
+//           >
+//             Contact<i className="fa fa-arrow-right ms-3"></i>
+//           </a>
+//         )}
+//       </div>
+//       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import logo from "./img/logo.webp";
 
 const Navbar: React.FC = () => {
@@ -10,12 +68,13 @@ const Navbar: React.FC = () => {
       className="navbar navbar-expand-lg bg-secondary navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn"
       data-wow-delay="0.1s"
     >
-      <a href="/" className="navbar-brand ms-4 ms-lg-0">
+      <Link to="/" className="navbar-brand ms-4 ms-lg-0">
         <h1 className="mb-0 text-primary text-uppercase">
           <img src={logo} alt="Sort Club Logo" width="20%" />
           SORT
         </h1>
-      </a>
+      </Link>
+
       <button
         type="button"
         className="navbar-toggler me-4"
@@ -24,21 +83,23 @@ const Navbar: React.FC = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
+
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <div className="navbar-nav ms-auto p-4 p-lg-0">
-          <a href="/" className="nav-item nav-link">
+          <Link to="/" className="nav-item nav-link">
             Home
-          </a>
-          <a href="about" className="nav-item nav-link">
+          </Link>
+          <Link to="/about" className="nav-item nav-link">
             About Us
-          </a>
-          <a href="events" className="nav-item nav-link">
+          </Link>
+          <Link to="/events" className="nav-item nav-link">
             Our Events
-          </a>
-          <a href="magazine" className="nav-item nav-link">
+          </Link>
+          <Link to="/magazine" className="nav-item nav-link">
             Magazine
-          </a>
+          </Link>
         </div>
+
         {location.pathname === "/" && (
           <a
             href="#contact"
@@ -48,7 +109,6 @@ const Navbar: React.FC = () => {
           </a>
         )}
       </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     </nav>
   );
 };
